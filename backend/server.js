313532f1c1988e018cbf49d3bfefe043e14e9fb5
amdonkamira-a11y <amdonkamira@gmail.com>
2026-07-22 15:47:42 +0300

    Fix bcryptjs vs bcrypt dependency mismatch crashing Render

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const { TikTokLiveConnection } = require("tiktok-live-connector");
 const crypto = require("crypto");
 
